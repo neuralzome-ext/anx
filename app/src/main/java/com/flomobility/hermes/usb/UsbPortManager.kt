8bf924454f4f812f202dc362b8fdd74e9ae9f5e8
Clay-FloMobility <clay@flomobility.com>
2022-09-13 15:10:46 +0530

    checks usb serial devices on app startup

--- app/src/main/java/com/flomobility/hermes/usb/UsbPortManager.kt
+++ app/src/main/java/com/flomobility/hermes/usb/UsbPortManager.kt
@@ -26,7 +26,6 @@ class UsbPortManager @Inject constructor(
 
     private val usbReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(arg0: Context, arg1: Intent) {
-            Timber.d("USB Listening on ${Thread.currentThread().name}")
             if (arg1.action == ACTION_USB_ATTACHED) {
                 val usbDevice = arg1.getParcelableExtra<UsbDevice>(UsbManager.EXTRA_DEVICE)
                 Timber.i("USB device attached $usbDevice")
@@ -34,30 +33,48 @@ class UsbPortManager @Inject constructor(
                     Timber.e("No usb device attached")
                     return
                 }
-                if (usbDevice.getDeviceType() == UsbDeviceType.SERIAL) {
-                    val serialPort = usbSerialManager.registerSerialDevice(usbDevice.deviceId)
-                    if(serialPort == -1) {
-                        Timber.e("No ports available for ${usbDevice.deviceName}")
-                        return
-                    }
-                    assetManager.addAsset(UsbSerial.create("$serialPort",usbDevice, usbManager))
-                }
+                usbDeviceCallback.onAttach(usbDevice)
             } else if (arg1.action == ACTION_USB_DETACHED) {
                 val usbDevice = arg1.getParcelableExtra<UsbDevice>(UsbManager.EXTRA_DEVICE)
                 if(usbDevice == null) {
                     Timber.e("No usb device detached")
                     return
                 }
-                if (usbDevice.getDeviceType() == UsbDeviceType.SERIAL) {
-                    val serialPort = usbSerialManager.unRegisterSerialDevice(usbDevice.deviceId)
-                    if (serialPort == -1) {
-                        Timber.e("Couldn't un-register ${usbDevice.deviceName}")
-                        return
-                    }
-                    assetManager.removeAsset("$serialPort", AssetType.USB_SERIAL)
-                }
+                usbDeviceCallback.onDetach(usbDevice)
                 Timber.d("$usbDevice disconnected")
             }
+        }
+    }
+
+    private val usbDeviceCallback = object : UsbDeviceCallback {
+        override fun onAttach(usbDevice: UsbDevice?) {
+            if (usbDevice?.getDeviceType() == UsbDeviceType.SERIAL) {
+                val serialPort = usbSerialManager.registerSerialDevice(usbDevice.deviceId)
+                if(serialPort == -1) {
+                    Timber.e("No ports available for ${usbDevice.deviceName}")
+                    return
+                }
+                assetManager.addAsset(UsbSerial.create("$serialPort",usbDevice, usbManager))
+            }
+        }
+
+        override fun onDetach(usbDevice: UsbDevice?) {
+            if (usbDevice?.getDeviceType() == UsbDeviceType.SERIAL) {
+                val serialPort = usbSerialManager.unRegisterSerialDevice(usbDevice.deviceId)
+                if (serialPort == -1) {
+                    Timber.e("Couldn't un-register ${usbDevice.deviceName}")
+                    return
+                }
+                assetManager.removeAsset("$serialPort", AssetType.USB_SERIAL)
+            }
+        }
+    }
+
+    private fun attachDevices() {
+        Timber.i("Looking for devices")
+        val devices = usbManager.deviceList ?: return
+        devices.values.sortedBy { it.deviceId }.forEach { usbDevice ->
+            usbDeviceCallback.onAttach(usbDevice)
         }
     }
 
@@ -66,8 +83,11 @@ class UsbPortManager @Inject constructor(
         filter.addAction(ACTION_USB_DETACHED)
         filter.addAction(ACTION_USB_ATTACHED)
         context.registerReceiver(usbReceiver, filter)
-    }
 
+//        if(usbSerialManager.isRegistryEmpty()) {
+            attachDevices()
+//        }
+    }
     companion object {
         private const val ACTION_USB_ATTACHED = UsbManager.ACTION_USB_DEVICE_ATTACHED
         private const val ACTION_USB_DETACHED = UsbManager.ACTION_USB_DEVICE_DETACHED
