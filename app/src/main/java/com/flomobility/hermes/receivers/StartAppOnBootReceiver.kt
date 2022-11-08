e2c1d6f40b562627baf6ec2948da0fdeb0977236
Clay-FloMobility <clay@flomobility.com>
2022-11-08 14:11:28 +0530

    access encrypted shared prefs in start app receiver

--- app/src/main/java/com/flomobility/hermes/receivers/StartAppOnBootReceiver.kt
+++ app/src/main/java/com/flomobility/hermes/receivers/StartAppOnBootReceiver.kt
@@ -4,22 +4,28 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import com.flomobility.hermes.di.AppModule
 import com.flomobility.hermes.other.getIsOnBoot
 import com.flomobility.hermes.ui.splash.SplashActivity
-import javax.inject.Inject
 
 
 class StartAppOnBootReceiver() : BroadcastReceiver() {
 
-    @Inject
-    lateinit var sharedPreferences: SharedPreferences
-
     override fun onReceive(context: Context?, intent: Intent?) {
+        val sharedPreferences = EncryptedSharedPreferences.create(
+            "user",
+            AppModule.masterKeyAlias,
+            context ?: return,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
         if (sharedPreferences.getIsOnBoot())
             if (Intent.ACTION_BOOT_COMPLETED == intent?.action) {
                 val i = Intent(context, SplashActivity::class.java)
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                context?.startActivity(i)
+                context.startActivity(i)
 //            runAsRoot(cmd = "am start com.flomobility.hermes/com.flomobility.hermes.MainActivity")
             }
     }
