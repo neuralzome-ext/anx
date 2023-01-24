5c5ba5589482c678ce3bab00b0e540db581190c8
Clay-FloMobility <clay@flomobility.com>
2023-01-25 02:39:49 +0530

    update IMU api. Closes #114

--- app/src/main/java/com/flomobility/anx/hermes/api/model/Quaternion.kt
+++ app/src/main/java/com/flomobility/anx/hermes/api/model/Quaternion.kt
@@ -4,4 +4,18 @@ data class Quaternion(
     val x: Double,
     val y: Double,
     val z: Double,
+    val w: Double
+)
+
+data class Vector3d(
+    val x: Double,
+    val y: Double,
+    val z: Double,
+)
+
+data class Vector4d(
+    val x: Double,
+    val y: Double,
+    val z: Double,
+    val w: Double
 )
