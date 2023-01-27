aa966aca5a2b7b1419368d5a9d79a7e76b7af6b4
Clay-FloMobility <clay@flomobility.com>
2023-01-27 15:08:14 +0530

    convert constant sleep to constant rate

--- app/src/main/java/com/flomobility/anx/hermes/common/Rate.kt
+++ app/src/main/java/com/flomobility/anx/hermes/common/Rate.kt
@@ -10,14 +10,35 @@ data class Rate(val hz: Int) {
         return (1_000L / hz)
     }
 
-    private var time = System.currentTimeMillis()
+    private var _start = System.currentTimeMillis()
+    private var _expected_cycle_time = 1000 / hz
+
+    private var actual_cycle_time = 0L
 
     fun sleep() {
-        val sleepDuration = (1000 / hz) - (System.currentTimeMillis() - time)
+/*        val sleepDuration = (1000 / hz) - (System.currentTimeMillis() - time)
         if (sleepDuration >= 1) {
             Thread.sleep(sleepDuration)
         }
-        time = System.currentTimeMillis()
-    }
+        time = System.currentTimeMillis()*/
+        var end = _start + _expected_cycle_time
+        val actualEnd = System.currentTimeMillis()
 
+        if (actualEnd < _start) {
+            end = actualEnd + _expected_cycle_time
+        }
+
+        val sleepDuration = end - actualEnd
+        actual_cycle_time = actualEnd - _start
+
+        _start = end
+        if(sleepDuration <= 0) {
+            if (actualEnd > end + _expected_cycle_time)
+            {
+                _start = actualEnd
+            }
+            return
+        }
+        Thread.sleep(sleepDuration)
+    }
 }
