da8fa9b4d350b22a520e02f76db6b5c1c306fdf3
Abhishek Majhi <abhishek@ealphabits.com>
2023-02-28 13:01:57 +0530

    Add set wifi RPC

--- device/src/main/java/com/flomobility/anx/rpc/SetWifiRpc.kt
+++ device/src/main/java/com/flomobility/anx/rpc/SetWifiRpc.kt
@@ -1,32 +1,23 @@
 package com.flomobility.anx.rpc
 
+import com.flomobility.anx.common.toStdResponse
 import com.flomobility.anx.proto.Common
 import com.flomobility.anx.proto.Device
+import com.flomobility.anx.wifi.WiFiManager
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class SetWifiRpc @Inject constructor() :
-    Rpc<Device.SetWifiRequest, Common.StdResponse>() {
-
-    private fun setWifi(): Common.StdResponse? {
-        val stdResponse = Common.StdResponse.newBuilder().apply {
-            this.success = true
-            this.message = "Wifi set up done"
-        }.build()
-        return stdResponse
-    }
+class SetWifiRpc @Inject constructor(
+    private val wiFiManager: WiFiManager
+) : Rpc<Device.SetWifiRequest, Common.StdResponse>() {
 
     override val name: String
         get() = "SetWifi"
 
     override fun execute(req: Device.SetWifiRequest): Common.StdResponse {
-        val stdResponse = Common.StdResponse.newBuilder()
-        stdResponse.apply {
-            success = setWifi()?.success!!
-            message = setWifi()?.message
-        }
-        return stdResponse.build()
+        val wifiStatus = wiFiManager.connectToWifi(req.ssid, req.password)
+        return wifiStatus.toStdResponse()
     }
 
     override fun execute(req: ByteArray): Common.StdResponse {
---
+++ device/src/main/java/com/flomobility/anx/wifi/WiFiManager.kt
@@ -0,0 +1,78 @@
+package com.flomobility.anx.wifi
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import android.net.wifi.WifiConfiguration
+import android.net.wifi.WifiManager
+import dagger.hilt.android.qualifiers.ApplicationContext
+import timber.log.Timber
+import com.flomobility.anx.common.Result
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Provides implementation of
+ * Wifi Connect/Disconnect.
+ */
+@Singleton
+class WiFiManager @Inject constructor(
+    @ApplicationContext var appContext: Context
+) {
+
+    private var wifiManager: WifiManager = appContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+    private var conManager: ConnectivityManager = appContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    private var networkInfo: NetworkInfo? = conManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+    private var isWifiConnected: Boolean = networkInfo != null && networkInfo!!.isConnected
+
+    companion object {
+        const val TAG = "WiFiManager"
+    }
+
+    /**
+     * method call on connect to wifi
+     */
+    fun connectToWifi(networkSSID: String?, networkPassword: String?): Result {
+        if (networkSSID.isNullOrEmpty() || networkPassword.isNullOrEmpty()) {
+            return Result(
+                success = false,
+                message = "Empty values are not allowed"
+            )
+        }
+        return connect(networkSSID, networkPassword)
+    }
+
+    private fun connect(networkSSID: String, networkPassword: String): Result {
+        try {
+            if (!wifiManager.isWifiEnabled) {
+                wifiManager.isWifiEnabled = true
+            }
+            if (wifiManager.isWifiEnabled) {
+                Timber.tag(TAG).d(".........")
+            }
+            removeAllNetworks()
+            val conf = WifiConfiguration()
+            conf.SSID = String.format("\"%s\"", networkSSID)
+            conf.preSharedKey = String.format("\"%s\"", networkPassword)
+            val netId = wifiManager.addNetwork(conf)
+            wifiManager.disconnect()
+            wifiManager.enableNetwork(netId, true)
+            wifiManager.reconnect()
+            return Result(true)
+        } catch (e: Exception) {
+            Timber.tag(TAG).d("Error connecting to $networkSSID : ${e.message}")
+            return Result(false, e.message.toString())
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    fun removeAllNetworks() {
+        val list = wifiManager.configuredNetworks
+        for (i in list) {
+            wifiManager.removeNetwork(i.networkId)
+            wifiManager.saveConfiguration();
+        }
+    }
+}
